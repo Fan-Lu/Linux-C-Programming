9160becbbbdf1eca8ee750349c1fa253893d2dfa
Fan-Lu <fanlu207@gmail.com>
2018-02-09 22:19:51 -0500

    Bug die

--- Combiner-Threads/combiner.c
+++ Combiner-Threads/combiner.c
@@ -10,19 +10,21 @@
 #include <stdlib.h>
 #include <sys/wait.h>
 #include <unistd.h>
+#include <string.h>
+#include <math.h>
 
-pthread_cond_t full;
-pthread_cond_t empty;
-pthread_mutex_t mtx;
-int N; //buffer size
-int M; //Num of item to insert, i.e. tuple input
-int num_user; //equals number of buffers
-char buffer[50][26];
-
-struct All_Pass{
-		char (*buff)[30];  //buffer pointer
-		int buff_size;
-};
+static pthread_cond_t full;
+static pthread_cond_t empty;
+static pthread_mutex_t mtx;
+int num_buffer; //number of buffers == number of reducers/users == number of comsumer threads
+int num_slots; //number of slots in each buffer
+char **buffer; //used to store the data from input
+int tuple_size = 30; 
+int whole_size; //whole size of each buffer
+int *pos;  //current position
+int *tup;  //num of tuples insert
+int length = 26;  //tuple length
+int num_insert = 0;
 
 void adjust(char A, char In[])
 {
@@ -76,36 +78,66 @@ void adjust(char A, char In[])
 
 void *mapper(void *param)  //Producer
 {
-		int line = 0;
-		char input[26];
-		char output[26];
+		char input[length];
+		char output[length];
 		char x;
-	
+		char user_id[num_buffer];  //use to identify which buffer to insert items
+		char ID; //Current ID
+
+		int j;
+		for(j=0; j<num_buffer; j++)
+		{
+				user_id[j] = j + '0';
+		}
+
 		FILE *pToFileForm = fopen("input.txt", "r");
 		FILE *f = fopen("mapper_output.txt", "w");
 
-		//printf("mappper_output.txt: \n");
-		while (fgets(input,26,pToFileForm))
+		printf("mappper_output.txt: \n");
+
+		pthread_mutex_lock(&mtx);
+		while(fgets(input,length,pToFileForm))
 		{
-			line++;
-			int i=26;
-			for(i;i--;i>0)
+			int i=length;
+			for(i=length;i--;i>0)
 			{
-				output[i]=input[i];
+				output[i] = input[i];
 			}
-		
 			char *add = &input[6];
 			x = *add;
+			ID = input[4];
 			adjust(x, output);
+		
+			for(i=0; i<num_buffer; i++)
+			{
+					if(ID == user_id[i])
+					{
+							for(j=pos[i]; j<(length+pos[i]); j++)
+							{
+									if(output[j-pos[i]] == '\n')   //detect the end of output tuple
+									{
+											buffer[i][j] = '\n';
+											pos[i] = j+1;
+											break;
+									}
+									else
+									{
+											buffer[i][j] = output[j-pos[i]];
+									}
+							}
+							tup[i]++;  //insert one tuple, will use it to compare to the num_slots
+							num_insert++;			//number of tuples inserted
+							
+					}
+			}
+			printf("%d\n", num_insert);
 			char *text = output;
-			//printf("%s", text);
-			//buffer[line] = output[0];
+			printf("%s", text);
 			fprintf(f, output, text);
 		}	
-	
+		pthread_mutex_unlock(&mtx);	
 		fclose(f);
 		fclose(pToFileForm);
-		//printf("\nover\n");
 		
 		pthread_exit(NULL);
 
@@ -113,64 +145,85 @@ void *mapper(void *param)  //Producer
 
 void *reducer(void *buf) //Comsumer
 {
-		//long tid;
-		//tid = (long)param;
-		//printf("Create reducer thread %ld\n", tid);
+		char save[50][50];
+		long buf_index;
+		buf_index = (long)buf;
 
-		struct All_Pass *my_buf;
-		my_buf = (struct All_Pass *)buf;
-		//printf("%d\n",my_buf->buff_size);
-
-		int i;
-		for(i=0; i<M; i++)  //M is the number of item to be inserted
+		int t=0;
+		int line;
+		for(; buffer[buf_index][t] != '\n'; t++)
 		{
-				pthread_mutex_lock(&mtx);
-				//printf("Buff Contex: %s\n", (*mybuf.buff)[0][0]);
-				//critical region
-				pthread_mutex_unlock(&mtx);
+				save[line][t] = buffer[buf_index][t];
+				t++;
+				save[line][t] = '0';
+				line++;
 		}
+
 		pthread_exit(NULL);
 }
 
 
 int main(int argc, char *argv[]) 
 {
+		int k,j;
+
 		if(argc == 1)
 		{
 				printf("Please Enter Input\n");
 		}
 		else
 		{
-				N = atoi(argv[1]); //buffer size
-				num_user = atoi(argv[2]); //num user
+				num_slots = atoi(argv[1]); //buffer size
+				num_buffer = atoi(argv[2]); //num use
 		}
 
-		char buffer[N][30];
-		struct All_Pass all_pass;
-		all_pass.buff = buffer;
-		//printf("%c\n", (all_pass.buff)[0][0]);
-		all_pass.buff_size = N;
+		int position[num_buffer], tuple_counter[num_buffer];
+
+		whole_size = 400;
+		/*create buffer for the communication between producer and consumer*/
+		buffer = (char**)malloc(sizeof(char*)*num_buffer); //assign number of bufferi
+		for(k=0; k<num_buffer; k++)
+		{
+				buffer[k] = (char*)malloc(sizeof(char)*whole_size); //assign the size of  space for each buffer
+		}
+		for(k=0; k<num_buffer; k++)  //initialize all the buffer
+		{
+				for(j=0; j<whole_size; j++)
+						buffer[k][j] = 'o';
+		}
+
+		/*Assign storage and initialize values*/
+		for(k=0; k<num_buffer; k++)
+		{
+				position[k] = 0;
+				tuple_counter[k] = 0;
+		}
+		pos = position;
+		tup = tuple_counter;
 
 		void *status;
 		pthread_attr_t attr;
 		pthread_mutex_init(&mtx, NULL);
-		pthread_t mapper_thread, reducer_thread[num_user];
+		pthread_t producer, consumer[num_buffer];
 
 		/*Create threads*/
 		pthread_attr_init(&attr);
 		pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
-		pthread_create(&mapper_thread, &attr, mapper, (void *)&all_pass);
+		/*Create producer thread;*/
+		pthread_create(&producer, &attr, mapper, NULL);
+		/*create consumer threads*/
 		long i;
-		for(i=0; i<num_user; i++)
+		for(i=0; i<num_buffer; i++)
 		{
-			pthread_create(&reducer_thread[i], &attr, reducer, (void *)&all_pass);
+			pthread_create(&consumer[i], &attr, reducer, (void *)i); //pass ith buffer to be handle by ith thread
 		}
 
 		/*wait on other threads*/
-		for(i=0; i<num_user; i++)
+		pthread_join(producer, &status);
+		for(i=0; i<num_buffer; i++)
 		{
-				pthread_join(reducer_thread[i], &status);
-				printf("main joined thread %ld\n", i);
+				pthread_join(consumer[i], &status);
+				//printf("main joined thread %ld\n", i);
 		}
 
 		pthread_mutex_destroy(&mtx);
